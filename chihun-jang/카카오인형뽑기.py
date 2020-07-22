c5cb0c8a88de3f4368d3e3c120c01970b451bad0
chichi <jang.chihun@gmail.com>
2020-07-22 19:28:11 +0900

    [개인-1] 카카오 인형뽑기 python 약간 수정 및 JS로 풀기

--- chihun-jang/카카오인형뽑기.py
+++ chihun-jang/카카오인형뽑기.py
@@ -11,13 +11,12 @@ def solution(board, moves):
                 
                 if stack_box[-1] == doll:
                     stack_box.pop()
-                    sub_list[i-1] = 0
                     answer += 2
                 else:  
                     stack_box.append(doll)
-                    sub_list[i-1] = 0
+                    
+                sub_list[i-1] = 0
                 break
-        
     return answer
 
 solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]],[1,5,3,5,1,2,1,4])
