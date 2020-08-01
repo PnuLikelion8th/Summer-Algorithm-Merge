2bdc90405e6a22346e6a91beb418a56f13c47270
xxhyowon <iios31@likelion.org>
2020-08-01 16:22:06 +0900

    [add] 2020-08-01 공통문제

---
+++ hyowon/2020-08-01.공통문제.py
@@ -0,0 +1,9 @@
+def solution(arr, divisor) :
+    a = []
+    for i in arr : 
+        if i % divisor == 0 :
+            a.append(i)
+        a.sort()
+    if len(a) == 0 :
+        a = [-1] 
+    return a
