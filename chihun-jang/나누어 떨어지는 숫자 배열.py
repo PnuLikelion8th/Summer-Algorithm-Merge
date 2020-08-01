f0e4f3813bf3f3eecb36f95d885381ded2b230e1
chichi <jang.chihun@gmail.com>
2020-08-01 23:56:01 +0900

    [공통 #3] 나누어 떨어지는 숫자배열 Python 코드 수정

--- chihun-jang/나누어 떨어지는 숫자 배열.py
+++ chihun-jang/나누어 떨어지는 숫자 배열.py
@@ -3,3 +3,9 @@ def solution(arr, divisor):
     if len(answer) ==0:
         answer.append(-1)
     return sorted(answer)
+
+def solution2(arr, divisor):
+    answer = [i for i in arr if i%divisor==0]
+    if not answer:
+        return [-1]
+    return sorted(answer)
