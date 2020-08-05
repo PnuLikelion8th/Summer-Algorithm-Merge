a9ef629430d276b708ff7b9ad2dc829ed4b54f67
dongyeopca <ckehdduq95@gmail.com>
2020-08-05 09:36:38 +0900

    개인문제

---
+++ dongyeop/2020.08.05.py
@@ -0,0 +1,23 @@
+
+def solution(participant, completion):
+    dictionary={}
+    total = 0
+    for i in participant:
+        dictionary[hash(i)]= i
+        total += hash(i)
+    for j in completion:
+        total -= hash(j)
+    return dictionary[total]
+
+print(solution(['leo','minsu','namin'],['minsu','namin']))
+
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[-1]
+
+
+print(solution(['alpha','minsu','namin','alpha'],['minsu','namin','alpha']))
