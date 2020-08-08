e16b1897d3bd682f643d612fe06ba5605295821c
mattew8 <mattew4483@likelion.org>
2020-08-08 22:56:06 +0900

    [add]0808공통문제

--- Lee/0805개인.py
+++ Lee/0805개인.py
@@ -4,19 +4,14 @@
 def solution(participant, completion):
     run = sorted(participant)
     win = sorted(completion)
-    # 우선 참가자와 완주자 리스트를 알파벳 순으로 정렬한다.
-
-    # win.append("X")
-    # => 완주자 리스트는 참여자보다 원소 하나가 모자라니 맨 뒤에 임의로 "X"를 넣었는데 없어도 잘 작동된다! 왜일까
-    # 중복 없을 경우 맨 마지막 참여자가 정답이면 win[i]는 아예 존재 안하는데 이러면 왜 run[i]!=win[i]가 충족되는감??
-    # 존재 하던 안하던 run[i]랑 다르니까 그냥 i를 return하는건가?.?
-
+    win.append("X")
     for i in range(len(run)):
         # run과 win이 순서대로 주르르륵 배열되어있으니 0번부터 끝까지 하나씩 비교해주면 그만이다
         if run[i]!=win[i]:
             return run[i]
             # 비교하다가 i번째 원소가 다르면 그 run[i]를 return하면 끝!
 
+print(solution(["a","b","c","b"],["a","b","c"]))
 # 밑에 적은 여러 뻘짓들은 답은 나오지만 효율성에서 빵점을 받는다.
 # 특히 for문 돌린 remove나 count는 효율성이 굉장히 떨어지는걸로 친다. 컴퓨터 바보
 # ----------------------------------------------------------------------------
