13ed3fb66f6f522ce86a3ebb5f370340f8b62313
phaesol <parkhaesol0227@likelion.org>
2020-08-19 21:07:48 +0900

    2020.08.19_개인_5번째.py

--- phaesol/2020.08.19_개인_5번째.py
+++ phaesol/2020.08.19_개인_5번째.py
@@ -6,5 +6,5 @@ def solution(array, commands):
         k = num[2]-1
         array_list = sorted(array[i:j])    #슬라이싱한 리스트를 정렬해주고
         result_list = array_list[k]        #우리가 원하는 순서인 k번째 수 리턴해주고
-        result.append(result_list)         # 그 숫자들을 리스트에다가 담아주기
-    return result
+        result.append(result_list)         # 그 숫자들을 리스트에다가 담아주고
+    return result                          # 담아준 리스트 리턴
